83765657f3f095903d87d30f15ae9a1f0c8c4af7
Kiki <rizki_andrianto@rocketmail.com>
2026-05-12 05:13:02 +0700

    Fix layout padding on mobile

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -258,7 +258,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
         {children}
       </main>
 
