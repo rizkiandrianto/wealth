f6693ff913b6db2a0926ec360a244177aaa8ba05
Kiki <rizki_andrianto@rocketmail.com>
2026-05-28 07:33:28 +0700

    Remova backup folder

--- drizzle.bak/0007_calm_wallop.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "crypto_sales" ADD COLUMN "purchase_date" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "gold_sales" ADD COLUMN "purchase_date" timestamp NOT NULL;--> statement-breakpoint
-ALTER TABLE "stock_sales" ADD COLUMN "purchase_date" timestamp NOT NULL;
